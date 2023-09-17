7c70bf4943475c67a1cce3e56c4b29314f5921b1
Rodion <rodiond26@gmail.com>
2023-09-17 12:41:27 +0500

    fix sql initial scripts

--- sql/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.grade_dict(grade_id, max_value, min_value, grade_text)
-VALUES
-(2, 0, 2.99, 'неуд'),
-(3, 3, 3.99, 'уд'),
-(4, 4, 4.99, 'хор'),
-(5, 5, 5, 'отл');
---
+++ src/main/resources/sql/001-schema.sql
@@ -0,0 +1,37 @@
+ALTER TABLE IF EXISTS public.students
+  DROP CONSTRAINT IF EXISTS students_fkey;
+
+DROP TABLE IF EXISTS public.students;
+DROP TABLE IF EXISTS public.grade_dict;
+
+CREATE TABLE IF NOT EXISTS public.grade_dict
+(
+    grade_id bigint NOT NULL,
+    grade_text character varying(255),
+    min_value double precision,
+    max_value double precision,
+    CONSTRAINT grade_dict_pkey PRIMARY KEY (grade_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.grade_dict
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.students
+(
+    student_id bigint NOT NULL,
+    full_name character varying(255),
+    date_birth date,
+    student_grade bigint,
+    CONSTRAINT students_pkey PRIMARY KEY (student_id),
+    CONSTRAINT students_fkey FOREIGN KEY (student_grade)
+        REFERENCES public.grade_dict (grade_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE SET NULL
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.students
+    OWNER to postgres;
