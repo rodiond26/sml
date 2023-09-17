0e766d05b1f664adce6a0cc05a803f3f5b47d6ae
Rodion <rodiond26@gmail.com>
2023-09-17 13:19:56 +0500

    fix sql

--- src/main/java/com/github/rodiond26/sml/entity/Student.java
+++ src/main/java/com/github/rodiond26/sml/entity/Student.java
@@ -1,5 +1,7 @@
 package com.github.rodiond26.sml.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,7 +17,7 @@ import java.time.LocalDate;
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "student_id")
     private Long id;
     @Column(name = "full_name")
@@ -23,8 +25,20 @@ public class Student {
     @Column(name = "date_birth")
     private LocalDate birthDate;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "student_grade")
     @ToString.Exclude
+    @JsonIgnore
     private Grade grade;
+
+    @JsonProperty(value = "grade")
+    public String getGrade() {
+        return this.grade != null
+                ? this.grade.getText()
+                : "";
+    }
+
+    @JsonIgnore
+    public void setGrade(String grade) {
+    }
 }
